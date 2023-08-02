7f58bb29cde95164105bedde2ee94279ee33e14c
长生 <64178985+JackeAstray@users.noreply.github.com>
2023-08-02 15:48:34 +0800

    移动文件位置

---
+++ EditorTools/Assets/ReunionMovement/LogTool/Runtime/Scripts/LogType.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLogic
+{
+    /// <summary>
+    /// 日志等级。
+    /// </summary>
+    public enum GameFrameworkLogLevel : byte
+    {
+        /// <summary>
+        /// 调试。
+        /// </summary>
+        Debug = 0,
+
+        /// <summary>
+        /// 信息。
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// 警告。
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// 错误。
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// 严重错误。
+        /// </summary>
+        Fatal
+    }
+
+
+}
