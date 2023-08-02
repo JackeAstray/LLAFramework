f162fe1187016aeeeb07817716890aa0d43bec02
长生 <64178985+JackeAstray@users.noreply.github.com>
2023-08-02 09:55:39 +0800

    首次更新

---
+++ EditorTools/Assets/LLATools/Editor/Scripts/OpenPath/OpenPath.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameLogic.Editor
+{
+    public static class OpenPath
+    {
+        ///// <summary>
+        ///// 打开 Data Path 文件夹。
+        ///// </summary>
+        //[MenuItem("工具箱/打开文件夹/Excel Tools", false, 10)]
+        //public static void OpenFolderExcelTools()
+        //{
+        //    Execute(Application.dataPath + "/../Config/ExcelTools");
+        //}
+
+        /// <summary>
+        /// 打开 Data Path 文件夹。
+        /// </summary>
+        [MenuItem("工具箱/打开文件夹/Data Path", false, 10)]
+        public static void OpenFolderDataPath()
+        {
+            Execute(Application.dataPath);
+        }
+
+        /// <summary>
+        /// 打开 Persistent Data Path 文件夹。
+        /// </summary>
+        [MenuItem("工具箱/打开文件夹/Persistent Data Path", false, 11)]
+        public static void OpenFolderPersistentDataPath()
+        {
+            Execute(Application.persistentDataPath);
+        }
+
+        /// <summary>
+        /// 打开 Streaming Assets Path 文件夹。
+        /// </summary>
+        [MenuItem("工具箱/打开文件夹/Streaming Assets Path", false, 12)]
+        public static void OpenFolderStreamingAssetsPath()
+        {
+            Execute(Application.streamingAssetsPath);
+        }
+
+        /// <summary>
+        /// 打开 Temporary Cache Path 文件夹。
+        /// </summary>
+        [MenuItem("工具箱/打开文件夹/Temporary Cache Path", false, 13)]
+        public static void OpenFolderTemporaryCachePath()
+        {
+            Execute(Application.temporaryCachePath);
+        }
+
+#if UNITY_2018_3_OR_NEWER
+
+        /// <summary>
+        /// 打开 Console Log Path 文件夹。
+        /// </summary>
+        [MenuItem("工具箱/打开文件夹/Console Log Path", false, 14)]
+        public static void OpenFolderConsoleLogPath()
+        {
+            Execute(System.IO.Path.GetDirectoryName(Application.consoleLogPath));
+        }
+
+#endif
+
+        /// <summary>
+        /// 打开指定路径的文件夹。
+        /// </summary>
+        /// <param name="folder">要打开的文件夹的路径。</param>
+        public static void Execute(string folder)
+        {
+            if (string.IsNullOrEmpty(folder)) return;
+
+            folder = string.Format("\"{0}\"", folder);
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsEditor:
+                    Process.Start("Explorer.exe", folder.Replace('/', '\\'));
+                    break;
+                case RuntimePlatform.OSXEditor:
+                    Process.Start("open", folder);
+                    break;
+                default:
+                    UnityEngine.Debug.LogError(string.Format("不支持在“｛0｝”平台上打开文件夹。", Application.platform));
+                    break;
+            }
+        }
+    }
+}
