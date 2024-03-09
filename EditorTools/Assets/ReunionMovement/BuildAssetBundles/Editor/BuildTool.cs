4ea01151ab0c44e4c0564388f19ac52063d70dfc
长生 <861276831@qq.com>
2024-03-09 17:26:53 +0800

    更新文件夹结构

--- EditorTools/Assets/ReunionMovement/BuildAssetBundles/Editor/BuildTool.cs
+++ EditorTools/Assets/ReunionMovement/BuildAssetBundles/Editor/BuildTool.cs
@@ -14,27 +14,24 @@ namespace GameLogic.EditorTools
         {
             // 获取ResourcesFile文件夹下的所有子文件夹
             string[] directoryEntries = Directory.GetDirectories("Assets/ResourcesFile");
-
+            // 获取当前的BuildTarget
             BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
-            
             // 导出路径
             string exportPath = Path.Combine(Application.dataPath, "../AssetBundles/"+ buildTarget);
-
+            // 如果导出路径不存在，则创建
             if (!Directory.Exists(exportPath))
             {
                 Directory.CreateDirectory(exportPath);
             }
 
             Log.Debug("导出路径: " + exportPath);
-
+            // 遍历所有子文件夹
             foreach (string directoryPath in directoryEntries)
             {
                 // 获取文件夹名称作为AB包的名称
                 string assetBundleName = Path.GetFileName(directoryPath);
-
                 // 设置AB包的名称
                 AssetImporter.GetAtPath(directoryPath).assetBundleName = assetBundleName;
-
                 // 打包AB包到指定路径
                 BuildPipeline.BuildAssetBundles("../AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
             }
