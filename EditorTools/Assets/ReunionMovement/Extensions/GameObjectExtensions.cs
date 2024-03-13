b67b9843057c26bfc7686a3644d1196ee647fe04
长生 <861276831@qq.com>
2024-03-13 17:14:40 +0800

    更新脚本

--- EditorTools/Assets/ReunionMovement/Extensions/GameObjectExtensions.cs
+++ EditorTools/Assets/ReunionMovement/Extensions/GameObjectExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 using Object = System.Object;
 
 namespace GameLogic
@@ -371,5 +372,108 @@ namespace GameLogic
         {
             ClearChild(go);
         }
+
+        #region UGUITool
+        /// <summary>
+        /// 设置文本内容
+        /// </summary>
+        /// <param name="textStr">文本内容</param>
+        /// <returns></returns>
+        public static Text SetText(this GameObject obj, string name, string textStr)
+        {
+            var text = obj.Get<Text>(name);
+            text.text = textStr;
+            return text;
+        }
+        /// <summary>
+        /// 设置按钮点击回调
+        /// </summary>
+        /// <param name="onClick">点击回调</param>
+        /// <returns></returns>
+        public static Button SetButton(this GameObject obj, string name, Action<GameObject> onClick)
+        {
+            var btn = obj.Get<Button>(name);
+            btn.onClick.AddListener(() =>
+            {
+                if (null != onClick)
+                {
+                    onClick(btn.gameObject);
+                }
+            });
+            return btn;
+        }
+
+        /// <summary>
+        /// 设置输入框文本输入完毕回调
+        /// </summary>
+        /// <param name="onEndEdit">文本输入完毕后回调</param>
+        /// <returns></returns>
+        public static InputField SetInputField(this GameObject obj, string name, Action<string> onEndEdit)
+        {
+            var input = obj.Get<InputField>(name);
+            input.onEndEdit.AddListener((v) =>
+            {
+                if (null != onEndEdit)
+                {
+                    onEndEdit(v);
+                }
+            });
+            return input;
+        }
+
+        /// <summary>
+        /// 设置下拉框选择回调
+        /// </summary>
+        /// <param name="onValueChanged">选择回调</param>
+        /// <returns></returns>
+        public static Dropdown SetDropDown(this GameObject obj, string name, Action<int> onValueChanged)
+        {
+            var dropdown = obj.Get<Dropdown>(name);
+            dropdown.onValueChanged.RemoveAllListeners();
+            dropdown.onValueChanged.AddListener((v) =>
+            {
+                if (null != onValueChanged)
+                    onValueChanged(v);
+            });
+
+            return dropdown;
+        }
+
+        /// <summary>
+        /// 设置单选框选择回调
+        /// </summary>
+        /// <param name="onValueChanged">选择回调</param>
+        /// <returns></returns>
+        public static Toggle SetToggle(this GameObject obj, string name, Action<bool> onValueChanged)
+        {
+            var toogle = obj.Get<Toggle>(name);
+            toogle.onValueChanged.RemoveAllListeners();
+            toogle.onValueChanged.AddListener((v) =>
+            {
+                if (null != onValueChanged)
+                    onValueChanged(v);
+            });
+
+            return toogle;
+        }
+
+        /// <summary>
+        /// 设置单选框选择回调
+        /// </summary>
+        /// <param name="onValueChanged">选择回调</param>
+        /// <returns></returns>
+        public static Slider SetSlider(this GameObject obj, string name, Action<float> onValueChanged)
+        {
+            var slider = obj.Get<Slider>(name);
+            slider.onValueChanged.RemoveAllListeners();
+            slider.onValueChanged.AddListener((v) =>
+            {
+                if (null != onValueChanged)
+                    onValueChanged(v);
+            });
+
+            return slider;
+        }
+        #endregion
     }
 }
---
+++ EditorTools/Assets/ReunionMovement/Toolbox/Tools/Screen/ScreenCapture/ScreenCapture.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLogic
+{
+    /// <summary>
+    /// 截屏
+    /// </summary>
+    public class ScreenCapture
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public static void Init()
+        {
+            for (int i = 0, len = s_camNameList.Length; i < len; ++i)
+            {
+                var camName = s_camNameList[i];
+                var camObj = GameObject.Find(camName);
+                if (null != camObj)
+                {
+                    var cam = camObj.GetComponent<Camera>();
+                    s_camList.Add(cam);
+                }
+                else
+                {
+                    Log.Error("摄像头不存在, 名称: " + camName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 截屏接口
+        /// </summary>
+        /// <returns></returns>
+        public static Texture2D StartScreenCapture()
+        {
+            int width = Screen.width;
+            int height = Screen.height;
+            RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 24);
+            for (int i = 0, cnt = s_camList.Count; i < cnt; ++i)
+            {
+                var cam = s_camList[i];
+                cam.targetTexture = rt;
+                cam.Render();
+            }
+            RenderTexture.active = rt;
+            Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+            tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            RenderTexture.active = null;
+            for (int i = 0, cnt = s_camList.Count; i < cnt; ++i)
+            {
+                var cam = s_camList[i];
+                cam.targetTexture = null;
+            }
+            Object.Destroy(rt);
+            tex.Apply();
+            return tex;
+        }
+
+        public static List<Camera> s_camList = new List<Camera>();
+
+        /// <summary>
+        /// 摄像机列表，注意顺序会影响渲染顺序，如果新增相机，要把相机添加到数组中
+        /// </summary>
+        private static string[] s_camNameList = new string[] { "Main Camera", "UICamera" };
+    }
+}
