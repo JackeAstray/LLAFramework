5019a97d700f3c9bba0e088912c4c0f795e1d7fa
长生 <jacke.yang@uccvr.com>
2024-10-24 17:45:41 +0800

    Update SkipSplashScreen.cs

--- LLAFramework/Assets/ReunionMovement/Common/Other/Runtime/SkipSplashScreen.cs
+++ LLAFramework/Assets/ReunionMovement/Common/Other/Runtime/SkipSplashScreen.cs
@@ -7,9 +7,16 @@ public class SkipSplashScreen
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     private static void Run()
     {
-        Task.Run(() =>
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
             SplashScreen.Stop(SplashScreen.StopBehavior.StopImmediate);
-        });
+        }
+        else
+        {
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                SplashScreen.Stop(SplashScreen.StopBehavior.StopImmediate);
+            });
+        }
     }
 }
