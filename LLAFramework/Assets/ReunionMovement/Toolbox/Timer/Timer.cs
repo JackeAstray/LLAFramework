28485893870244683373d1210b693cc0af6756a2
长生 <jacke.yang@uccvr.com>
2025-02-20 11:44:37 +0800

    Update Timer.cs

--- LLAFramework/Assets/ReunionMovement/Toolbox/Timer/Timer.cs
+++ LLAFramework/Assets/ReunionMovement/Toolbox/Timer/Timer.cs
@@ -14,7 +14,6 @@ namespace GameLogic
     public class Timer
     {
         #region 公共 属性/字段
-
         /// <summary>
         /// 计时器从开始到结束需要多长时间。
         /// </summary>
@@ -40,7 +39,10 @@ namespace GameLogic
         /// </summary>
         public bool isPaused
         {
-            get { return this._timeElapsedBeforePause.HasValue; }
+            get
+            {
+                return this.timeElapsedBeforePause.HasValue;
+            }
         }
 
         /// <summary>
@@ -48,7 +50,10 @@ namespace GameLogic
         /// </summary>
         public bool isCancelled
         {
-            get { return this._timeElapsedBeforeCancel.HasValue; }
+            get
+            {
+                return this.timeElapsedBeforeCancel.HasValue;
+            }
         }
 
         /// <summary>
@@ -56,13 +61,143 @@ namespace GameLogic
         /// </summary>
         public bool isDone
         {
-            get { return this.isCompleted || this.isCancelled || this.isOwnerDestroyed; }
+            get
+            {
+                return this.isCompleted || this.isCancelled || this.isOwnerDestroyed;
+            }
+        }
+        #endregion
+
+        #region 私有静态 属性/字段
+
+        // 负责更新所有注册的定时器
+        private static TimerManager manager;
+        #endregion
+
+        #region 私有 属性/字段
+        private bool isOwnerDestroyed
+        {
+            get { return this.hasAutoDestroyOwner && this.autoDestroyOwner == null; }
         }
 
+        private readonly Action onComplete;
+        private readonly Action<float> onUpdate;
+        private float startTime;
+        private float lastUpdateTime;
+
+        // 对于暂停，我们将开始时间提前一段时间。
+        // 如果我们只检查开始时间与当前世界时间的对比，
+        // 那么当我们被取消或暂停时，
+        // 所花费的时间就会变得混乱，
+        // 所以我们需要缓存在暂停/取消之前所花费的
+        private float? timeElapsedBeforeCancel;
+        private float? timeElapsedBeforePause;
+
+        // 在自动销毁所有者被销毁后，
+        // 计时器将过期，
+        // 这样你就不会遇到任何恼人的错误，
+        // 因为计时器在对象被销毁后运行和访问对象
+        private readonly MonoBehaviour autoDestroyOwner;
+        private readonly bool hasAutoDestroyOwner;
+        #endregion
+
+        #region 公共方法
+
+        /// <summary>
+        /// 停止正在进行或暂停的计时器。计时器的完成时回调将不会被调用。
+        /// </summary>
+        public void Cancel()
+        {
+            if (this.isDone)
+            {
+                return;
+            }
+
+            this.timeElapsedBeforeCancel = this.GetTimeElapsed();
+            this.timeElapsedBeforePause = null;
+        }
+
+        /// <summary>
+        /// 暂停正在运行的计时器。暂停的计时器可以从暂停的同一点恢复。
+        /// </summary>
+        public void Pause()
+        {
+            if (this.isPaused || this.isDone)
+            {
+                return;
+            }
+
+            this.timeElapsedBeforePause = this.GetTimeElapsed();
+        }
+
+        /// <summary>
+        /// 继续暂停计时器。如果计时器未暂停，则不执行任何操作。
+        /// </summary>
+        public void Resume()
+        {
+            if (!this.isPaused || this.isDone)
+            {
+                return;
+            }
+
+            this.timeElapsedBeforePause = null;
+        }
+
+        /// <summary>
+        /// 获取此计时器当前周期开始后经过的秒数。
+        /// </summary>
+        /// <returns>
+        /// 自该计时器的当前循环开始以来所经过的秒数，即，如果计时器已循环，则为当前循环，如果计时器未循环则为启动。
+        ///
+        /// 如果计时器已经结束运行，则这等于持续时间。
+        ///
+        /// 如果计时器被取消/暂停，则等于从计时器启动到取消/暂停之间经过的秒数。
+        /// </returns>
+        public float GetTimeElapsed()
+        {
+            if (this.isCompleted || this.GetWorldTime() >= this.GetFireTime())
+            {
+                return this.duration;
+            }
+
+            return this.timeElapsedBeforeCancel ??
+                   this.timeElapsedBeforePause ??
+                   this.GetWorldTime() - this.startTime;
+        }
+
+        /// <summary>
+        /// 获取距离计时器完成还有多少秒。
+        /// </summary>
+        /// <returns>
+        /// 计时器完成之前剩余的秒数。
+        /// 计时器只有在未暂停、取消或完成的情况下才会经过时间。
+        /// 如果计时器完成，这将等于零。
+        /// </returns>
+        public float GetTimeRemaining()
+        {
+            return duration - GetTimeElapsed();
+        }
+
+        /// <summary>
+        /// 以比率的形式获取计时器从开始到结束的进度。
+        /// </summary>
+        /// <returns>一个从0到1的值，指示计时器的持续时间已经过去了多少.</returns>
+        public float GetRatioComplete()
+        {
+            return GetTimeElapsed() / duration;
+        }
+
+        /// <summary>
+        /// 以比率的形式获取计时器剩余的进度。
+        /// </summary>
+        /// <returns>一个从0到1的值，指示计时器的持续时间还有多长。</returns>
+        public float GetRatioRemaining()
+        {
+            return GetTimeRemaining() / duration;
+        }
         #endregion
 
         #region 公共静态方法
-
         /// <summary>
         /// 注册一个新的计时器，该计时器应在经过一定时间后触发事件。
         ///
@@ -83,26 +218,31 @@ namespace GameLogic
         /// <returns>
         /// 一个计时器对象，允许您检查统计数据并停止/恢复进度。
         /// </returns>
-        public static Timer Register(float duration, Action onComplete, Action<float> onUpdate = null,
-            bool isLooped = false, bool useRealTime = false, MonoBehaviour autoDestroyOwner = null)
+        public static Timer Register(float duration,
+                                     Action onComplete,
+                                     Action<float> onUpdate = null,
+                                     bool isLooped = false,
+                                     bool useRealTime = false,
+                                     MonoBehaviour autoDestroyOwner = null
+        )
         {
             // 创建一个管理器对象来更新所有计时器（如果还不存在）。
-            if (Timer._manager == null)
+            if (Timer.manager == null)
             {
                 TimerManager managerInScene = Object.FindObjectOfType<TimerManager>();
                 if (managerInScene != null)
                 {
-                    Timer._manager = managerInScene;
+                    Timer.manager = managerInScene;
                 }
                 else
                 {
                     GameObject managerObject = new GameObject { name = "TimerManager" };
-                    Timer._manager = managerObject.AddComponent<TimerManager>();
+                    Timer.manager = managerObject.AddComponent<TimerManager>();
                 }
             }
 
             Timer timer = new Timer(duration, onComplete, onUpdate, isLooped, useRealTime, autoDestroyOwner);
-            Timer._manager.RegisterTimer(timer);
+            Timer.manager.RegisterTimer(timer);
             return timer;
         }
 
@@ -144,195 +284,66 @@ namespace GameLogic
                 timer.Resume();
             }
         }
+
         /// <summary>
         /// 取消所有注册计时器
         /// </summary>
         public static void CancelAllRegisteredTimers()
         {
-            if (Timer._manager != null)
+            if (Timer.manager != null)
             {
-                Timer._manager.CancelAllTimers();
+                Timer.manager.CancelAllTimers();
             }
-
             // 如果管理员不存在，我们还没有任何注册的计时器，所以在这种情况下不需要做任何事情
         }
+
         /// <summary>
         /// 暂停所有注册的计时器
         /// </summary>
         public static void PauseAllRegisteredTimers()
         {
-            if (Timer._manager != null)
+            if (Timer.manager != null)
             {
-                Timer._manager.PauseAllTimers();
+                Timer.manager.PauseAllTimers();
             }
-
             // 如果管理员不存在，我们还没有任何注册的计时器，所以在这种情况下不需要做任何事情
         }
+
         /// <summary>
         /// 恢复所有注册的计时器
         /// </summary>
         public static void ResumeAllRegisteredTimers()
         {
-            if (Timer._manager != null)
+            if (Timer.manager != null)
             {
-                Timer._manager.ResumeAllTimers();
+                Timer.manager.ResumeAllTimers();
             }
-
             // 如果管理员不存在，我们还没有任何注册的计时器，所以在这种情况下不需要做任何事情
         }
-
-        #endregion
-
-        #region 公共方法
-
-        /// <summary>
-        /// 停止正在进行或暂停的计时器。计时器的完成时回调将不会被调用。
-        /// </summary>
-        public void Cancel()
-        {
-            if (this.isDone)
-            {
-                return;
-            }
-
-            this._timeElapsedBeforeCancel = this.GetTimeElapsed();
-            this._timeElapsedBeforePause = null;
-        }
-
-        /// <summary>
-        /// 暂停正在运行的计时器。暂停的计时器可以从暂停的同一点恢复。
-        /// </summary>
-        public void Pause()
-        {
-            if (this.isPaused || this.isDone)
-            {
-                return;
-            }
-
-            this._timeElapsedBeforePause = this.GetTimeElapsed();
-        }
-
-        /// <summary>
-        /// 继续暂停计时器。如果计时器未暂停，则不执行任何操作。
-        /// </summary>
-        public void Resume()
-        {
-            if (!this.isPaused || this.isDone)
-            {
-                return;
-            }
-
-            this._timeElapsedBeforePause = null;
-        }
-
-        /// <summary>
-        /// 获取此计时器当前周期开始后经过的秒数。
-        /// </summary>
-        /// <returns>
-        /// 自该计时器的当前循环开始以来所经过的秒数，即，如果计时器已循环，则为当前循环，如果计时器未循环则为启动。
-        ///
-        /// 如果计时器已经结束运行，则这等于持续时间。
-        ///
-        /// 如果计时器被取消/暂停，则等于从计时器启动到取消/暂停之间经过的秒数。
-        /// </returns>
-        public float GetTimeElapsed()
-        {
-            if (this.isCompleted || this.GetWorldTime() >= this.GetFireTime())
-            {
-                return this.duration;
-            }
-
-            return this._timeElapsedBeforeCancel ??
-                   this._timeElapsedBeforePause ??
-                   this.GetWorldTime() - this._startTime;
-        }
-
-        /// <summary>
-        /// 获取距离计时器完成还有多少秒。
-        /// </summary>
-        /// <returns>
-        /// 计时器完成之前剩余的秒数。
-        /// 计时器只有在未暂停、取消或完成的情况下才会经过时间。
-        /// 如果计时器完成，这将等于零。
-        /// </returns>
-        public float GetTimeRemaining()
-        {
-            return this.duration - this.GetTimeElapsed();
-        }
-
-        /// <summary>
-        /// 以比率的形式获取计时器从开始到结束的进度。
-        /// </summary>
-        /// <returns>一个从0到1的值，指示计时器的持续时间已经过去了多少.</returns>
-        public float GetRatioComplete()
-        {
-            return this.GetTimeElapsed() / this.duration;
-        }
-
-        /// <summary>
-        /// 以比率的形式获取计时器剩余的进度。
-        /// </summary>
-        /// <returns>一个从0到1的值，指示计时器的持续时间还有多长。</returns>
-        public float GetRatioRemaining()
-        {
-            return this.GetTimeRemaining() / this.duration;
-        }
-
-        #endregion
-
-        #region 私有静态 属性/字段
-
-        // 负责更新所有注册的定时器
-        private static TimerManager _manager;
-
-        #endregion
-
-        #region 私有 属性/字段
-
-        private bool isOwnerDestroyed
-        {
-            get { return this._hasAutoDestroyOwner && this._autoDestroyOwner == null; }
-        }
-
-        private readonly Action _onComplete;
-        private readonly Action<float> _onUpdate;
-        private float _startTime;
-        private float _lastUpdateTime;
-
-        // 对于暂停，我们将开始时间提前一段时间。
-        // 如果我们只检查开始时间与当前世界时间的对比，
-        // 那么当我们被取消或暂停时，
-        // 所花费的时间就会变得混乱，
-        // 所以我们需要缓存在暂停/取消之前所花费的
-        private float? _timeElapsedBeforeCancel;
-        private float? _timeElapsedBeforePause;
-
-        // 在自动销毁所有者被销毁后，
-        // 计时器将过期，
-        // 这样你就不会遇到任何恼人的错误，
-        // 因为计时器在对象被销毁后运行和访问对象
-        private readonly MonoBehaviour _autoDestroyOwner;
-        private readonly bool _hasAutoDestroyOwner;
-
         #endregion
 
         #region 私有构造函数（使用静态Register方法创建新计时器）
 
-        private Timer(float duration, Action onComplete, Action<float> onUpdate,
-            bool isLooped, bool usesRealTime, MonoBehaviour autoDestroyOwner)
+        private Timer(float duration,
+                      Action onComplete,
+                      Action<float> onUpdate,
+                      bool isLooped,
+                      bool usesRealTime,
+                      MonoBehaviour autoDestroyOwner
+        )
         {
             this.duration = duration;
-            this._onComplete = onComplete;
-            this._onUpdate = onUpdate;
+            this.onComplete = onComplete;
+            this.onUpdate = onUpdate;
 
             this.isLooped = isLooped;
             this.usesRealTime = usesRealTime;
 
-            this._autoDestroyOwner = autoDestroyOwner;
-            this._hasAutoDestroyOwner = autoDestroyOwner != null;
+            this.autoDestroyOwner = autoDestroyOwner;
+            this.hasAutoDestroyOwner = autoDestroyOwner != null;
 
-            this._startTime = this.GetWorldTime();
-            this._lastUpdateTime = this._startTime;
+            this.startTime = this.GetWorldTime();
+            this.lastUpdateTime = this.startTime;
         }
 
         #endregion
@@ -344,15 +355,16 @@ namespace GameLogic
         /// <returns></returns>
         private float GetWorldTime()
         {
-            return this.usesRealTime ? Time.realtimeSinceStartup : Time.time;
+            return usesRealTime ? Time.realtimeSinceStartup : Time.time;
         }
+
         /// <summary>
         /// 获取点火时间
         /// </summary>
         /// <returns></returns>
         private float GetFireTime()
         {
-            return this._startTime + this.duration;
+            return startTime + duration;
         }
 
         /// <summary>
@@ -361,7 +373,7 @@ namespace GameLogic
         /// <returns></returns>
         private float GetTimeDelta()
         {
-            return this.GetWorldTime() - this._lastUpdateTime;
+            return GetWorldTime() - lastUpdateTime;
         }
 
         /// <summary>
@@ -369,58 +381,46 @@ namespace GameLogic
         /// </summary>
         private void Update()
         {
-            if (this.isDone)
+            if (isDone) return;
+
+            if (isPaused)
             {
+                startTime += GetTimeDelta();
+                lastUpdateTime = GetWorldTime();
                 return;
             }
 
-            if (this.isPaused)
+            lastUpdateTime = GetWorldTime();
+
+            onUpdate?.Invoke(GetTimeElapsed());
+
+            if (GetWorldTime() >= GetFireTime())
             {
-                this._startTime += this.GetTimeDelta();
-                this._lastUpdateTime = this.GetWorldTime();
-                return;
-            }
+                onComplete?.Invoke();
 
-            this._lastUpdateTime = this.GetWorldTime();
-
-            if (this._onUpdate != null)
-            {
-                this._onUpdate(this.GetTimeElapsed());
-            }
-
-            if (this.GetWorldTime() >= this.GetFireTime())
-            {
-
-                if (this._onComplete != null)
+                if (isLooped)
                 {
-                    this._onComplete();
-                }
-
-                if (this.isLooped)
-                {
-                    this._startTime = this.GetWorldTime();
+                    startTime = GetWorldTime();
                 }
                 else
                 {
-                    this.isCompleted = true;
+                    isCompleted = true;
                 }
             }
         }
-
         #endregion
 
         #region Manager类（实现细节，自动生成并更新所有注册的定时器）
-
         /// <summary>
         /// 管理更新应用程序中运行的所有<see cref=“Timer”/>。
         /// 这将在您第一次创建计时器时实例化——您不需要手动将其添加到场景中。
         /// </summary>
         private class TimerManager : MonoBehaviour
         {
-            private List<Timer> _timers = new List<Timer>();
+            private List<Timer> timers = new List<Timer>();
 
             // 缓冲区添加计时器，这样我们就不会在迭代过程中编辑集合
-            private List<Timer> _timersToAdd = new List<Timer>();
+            private List<Timer> timersToAdd = new List<Timer>();
 
             /// <summary>
             /// 注册计时器
@@ -428,7 +428,7 @@ namespace GameLogic
             /// <param name="timer"></param>
             public void RegisterTimer(Timer timer)
             {
-                this._timersToAdd.Add(timer);
+                timersToAdd.Add(timer);
             }
 
             /// <summary>
@@ -436,13 +436,13 @@ namespace GameLogic
             /// </summary>
             public void CancelAllTimers()
             {
-                foreach (Timer timer in this._timers)
+                foreach (Timer timer in timers)
                 {
                     timer.Cancel();
                 }
 
-                this._timers.Clear();
-                this._timersToAdd.Clear();
+                timers.Clear();
+                timersToAdd.Clear();
             }
 
             /// <summary>
@@ -450,7 +450,7 @@ namespace GameLogic
             /// </summary>
             public void PauseAllTimers()
             {
-                foreach (Timer timer in this._timers)
+                foreach (Timer timer in timers)
                 {
                     timer.Pause();
                 }
@@ -461,7 +461,7 @@ namespace GameLogic
             /// </summary>
             public void ResumeAllTimers()
             {
-                foreach (Timer timer in this._timers)
+                foreach (Timer timer in timers)
                 {
                     timer.Resume();
                 }
@@ -473,7 +473,7 @@ namespace GameLogic
             [UsedImplicitly]
             private void Update()
             {
-                this.UpdateAllTimers();
+                UpdateAllTimers();
             }
 
             /// <summary>
@@ -481,22 +481,20 @@ namespace GameLogic
             /// </summary>
             private void UpdateAllTimers()
             {
-                if (this._timersToAdd.Count > 0)
+                if (timersToAdd.Count > 0)
                 {
-                    this._timers.AddRange(this._timersToAdd);
-                    this._timersToAdd.Clear();
+                    timers.AddRange(timersToAdd);
+                    timersToAdd.Clear();
                 }
 
-                foreach (Timer timer in this._timers)
+                foreach (Timer timer in timers)
                 {
                     timer.Update();
                 }
 
-                this._timers.RemoveAll(t => t.isDone);
+                timers.RemoveAll(t => t.isDone);
             }
         }
-
         #endregion
-
     }
 }
