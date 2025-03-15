5483672abbefc8eefa48e3404458744d4bf3090f
长生 <jacke.yang@uccvr.com>
2025-03-15 14:48:51 +0800

    Update UnityTypeBindings.cs

--- LLAFramework/Assets/ReunionMovement/Common/LitJson/UnityTypeBindings.cs
+++ LLAFramework/Assets/ReunionMovement/Common/LitJson/UnityTypeBindings.cs
@@ -150,7 +150,57 @@ namespace LitJson
                 w.WriteObjectEnd();
             });
 
-        }
+            // 注册Matrix4x4类型的Exporter
+            JsonMapper.RegisterExporter<Matrix4x4>((v, w) =>
+            {
+                w.WriteObjectStart();
+                w.WriteProperty("m00", v.m00);
+                w.WriteProperty("m01", v.m01);
+                w.WriteProperty("m02", v.m02);
+                w.WriteProperty("m03", v.m03);
+                w.WriteProperty("m10", v.m10);
+                w.WriteProperty("m11", v.m11);
+                w.WriteProperty("m12", v.m12);
+                w.WriteProperty("m13", v.m13);
+                w.WriteProperty("m20", v.m20);
+                w.WriteProperty("m21", v.m21);
+                w.WriteProperty("m22", v.m22);
+                w.WriteProperty("m23", v.m23);
+                w.WriteProperty("m30", v.m30);
+                w.WriteProperty("m31", v.m31);
+                w.WriteProperty("m32", v.m32);
+                w.WriteProperty("m33", v.m33);
+                w.WriteObjectEnd();
+            });
 
+            // 注册LayerMask类型的Exporter
+            JsonMapper.RegisterExporter<LayerMask>((v, w) =>
+            {
+                w.WriteObjectStart();
+                w.WriteProperty("value", v.value);
+                w.WriteObjectEnd();
+            });
+
+            // 注册AnimationCurve类型的Exporter
+            JsonMapper.RegisterExporter<AnimationCurve>((v, w) =>
+            {
+                w.WriteObjectStart();
+                w.WritePropertyName("keys");
+                w.WriteArrayStart();
+                foreach (var key in v.keys)
+                {
+                    w.WriteObjectStart();
+                    w.WriteProperty("time", key.time);
+                    w.WriteProperty("value", key.value);
+                    w.WriteProperty("inTangent", key.inTangent);
+                    w.WriteProperty("outTangent", key.outTangent);
+                    w.WriteObjectEnd();
+                }
+                w.WriteArrayEnd();
+                w.WriteProperty("preWrapMode", v.preWrapMode.ToString());
+                w.WriteProperty("postWrapMode", v.postWrapMode.ToString());
+                w.WriteObjectEnd();
+            });
+        }
     }
 }
